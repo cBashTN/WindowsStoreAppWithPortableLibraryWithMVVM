51d8add88e70f0f821ccb07b077da5ac8d98220f
cBashTN <sebastian.mojado@gmail.com>
2013-01-07 17:00:04 +0100

    Initial Commit

---
+++ SimpleMVVM/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleMVVM.Model
+{
+    public class Customer
+    {
+        public int CustomerID
+        {
+            get;
+            set;
+        }
+
+        public string FullName
+        {
+            get;
+            set;
+        }
+
+        public string Phone
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ SimpleMVVM/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SimpleMVVM.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
